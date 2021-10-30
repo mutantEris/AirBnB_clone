b084a1366599d625f0653bc2eb8ac294027e4e00
1107tomkim <cryptotaekim@gmail.com>
2021-10-30 03:25:11 +0000

    Adding what we have of engine files

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Module used to store objects into Json file
+'''
+
+import models
+import json
+
+class FileStorage:
+    '''Class FileStorage'''
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''Returns __objects'''
+
+        return self.__objects
+
+    def new(self, obj):
+        '''class new'''
+
+        key = obj.__class__.__name__. + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        '''class save'''
+
+        new_dict = {}
+
+    def reload(self):
+        '''class reload'''
