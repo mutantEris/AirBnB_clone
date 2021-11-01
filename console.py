c469784cb9bddcbf29fec0b67daada04560d53bd
mutantEris <mmires@live.com>
2021-11-01 08:47:55 -0700

    push for safety

--- console.py
+++ console.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 """this is, in fact, the console"""
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """command prompt"""
     prompt = "(hbnb)"
 
+    def do_create(self, args):
+        """Creates new instance of BaseModel"""
+        if args == "" or None:
+            print("** class name missing **")
+        elif args != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            base = BaseModel()
+            print(base.id)
+
+    def do_show(self, args):
+        """Shows a specific BaseModel instance"""
+        if args == "" or None:
+            print("** class name missing **")
     def do_quit(self, args):
         """Quits Console"""
         exit()
