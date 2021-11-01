641634fba8821a8b5880b9fcc3f57fc629a33f76
1107tomkim <cryptotaekim@gmail.com>
2021-11-01 18:36:29 +0000

    Fixing typo of updated_at from update_at

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.update_at = datetime.now()
+            self.updated_at = datetime.now()
 
         models.storage.new(self)
 
