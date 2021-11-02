4de7a70fcc3c2fadc655668317e29c0beaeacb90
1107tomkim <cryptotaekim@gmail.com>
2021-11-02 04:50:56 +0000

    Creating Class City

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module defining class City'''
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''Class City'''
+    name = ""
+    state_id = ""
