7220464494d720a2a848522d7c4b0dcd2f103930
1107tomkim <cryptotaekim@gmail.com>
2021-11-02 04:41:30 +0000

    Class User inherits from BaseModel

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module defining class user'''
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    '''Class user'''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
