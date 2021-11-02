c925b25769090c34a951c590c335b003e0d4833b
1107tomkim <cryptotaekim@gmail.com>
2021-11-02 04:08:38 +0000

    Please work please work please work

--- tests/test_all.py
+++ tests/test_all.py
@@ -22,3 +22,25 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(tbm.created_at, datetime.datetime)
         self.assertIsInstance(tbm.updated_at, datetime.datetime)
         self.assertIsInstance(tbm.to_dict(), dict)
+
+    def TestKwargs(self):
+        ''' Test Kwargs'''
+        temp_dict = {"updated_at": "2021-11-01T22:14:420127",
+                    "created_at": "2021-11-01T22:14:420127",
+                    "id": "123abc",
+                    "__class__": "BaseModel"}
+        mbt = BaseModel(**temp_dict)
+        self.assertEqual(mbt.id, "123abc")
+        self.assertIsInstance(mbt, BaseModel)
+        self.assertIsInstance(mbt.created_at, datetime.datetime)
+        self.assertIsInstance(mbt.updated_at, datetime.datetime)
+        self.assertIsInstance(mbt.to_dct(), dict)
+
+    def TestStr(self):
+        ''' Test Str '''
+        tmb = BaseModel()
+        self.assertEqual(str(tmb), "[BaseModel] ({}) {}".format
+                                    (b.id, b.__dict__))
+        
+if __name__ == '__main__':
+    unittest.main()
