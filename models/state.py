4833df9772ceb864f414cb4c39227e28cba28ade
1107tomkim <cryptotaekim@gmail.com>
2021-11-02 04:50:41 +0000

    Creating class State

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module defining class State'''
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''Class State inherited from BaseModel'''
+    name = ""
