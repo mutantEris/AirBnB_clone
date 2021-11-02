1933ead522bcf6a73f85c9ea3d72fb0f883735b6
1107tomkim <cryptotaekim@gmail.com>
2021-11-02 00:01:44 +0000

    Adding updated test file of basemodel, merging into 1 method

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
 '''Test All Unittest'''
 
+
 import unittest
 import datetime
 import json
 from models.base_model.py import Basemodel
+import os
+
 
 class TestBaseModel(unittest.TestCase):
     '''Test for BaseModel'''
 
-    def test_tbm_exists(self):
-    ''' Test to see if tbm was created as an instance'''
-        self.tbm = BaseModel()
-        self.assertTrue(tbm)
-
-    def test_tbm_equal_BaseModel(self):
-    ''' Test to see if tbm is equal to BaseModel'''
-        self.assertIsInstance(self.tbm, BaseModel)
-
-    def test_ids(self):
-    ''' test ids '''
-        self.mbt = BaseModel()
-        self.assertNotEqual(self.tbm.id, self.mbt.id)
-
-    def test_created_at(self):
-    ''' test created_at '''
-        self.assertIsInstance(self.tbm.created_at, datetime.datetime)
-
-    def test_updated_at(self):
-    ''' testing updated_at '''
-        self.assertIsInstance(self.tbm.updated_at, datetime.datetime)
-
-    def test_str_(self):
-    ''' test string '''
-
-    def test_dict(self):
-    ''' test dict'''
+    def test_tbm(self):
+        ''' Test BaseModel'''
+        tbm = BaseModel()
+        selfassertIsInstance(tbm.id, str)
+        tbm.id = 5
+        self.assertIsInstance(tbm, BaseModel)
+        self.assertEqual(5, tbm.id)
+        self.assertIsInstance(tbm.created_at, datetime.datetime)
+        self.assertIsInstance(tbm.updated_at, datetime.datetime)
+        self.assertIsInstance(tbm.to_dict(), dict)
